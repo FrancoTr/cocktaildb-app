5826849a591ed5da37445abd48d88cc8c29e4975
Franco Traverso <franco-t20@outlook.com>
2021-09-19 00:18:51 -0300

    add logo to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,14 @@ import logo from "../logo.svg";
 
 const Navbar = () => {
   return (
-    <div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <img src={logo} alt="cocktail db logo" className="logo"></img>
+        </Link>
+      </div>
       <h2>navbar component</h2>
-    </div>
+    </nav>
   );
 };
 
