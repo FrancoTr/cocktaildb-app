937695c803c0bf3dc7972dd8595c60e59ae22741
Franco Traverso <franco-t20@outlook.com>
2021-09-20 20:39:16 -0300

    add About page

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur saepe officiis id quas?
+        Sint repellat, facere porro optio delectus sapiente. Aperiam illo non, maxime repudiandae
+        nisi consequatur tempora eaque, dolor impedit ducimus, accusamus sequi ea ut facere nemo
+        magni voluptatum.
+      </p>
+    </section>
+  );
+};
+
+export default About;
