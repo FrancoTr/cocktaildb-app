6272e97dec6e966dffd5916c0db6aac0f9fbc038
Franco Traverso <franco-t20@outlook.com>
2021-09-23 20:34:00 -0300

    add useEffect hook, called everytime the cocktail id changes

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -9,6 +9,37 @@ const SingleCocktail = () => {
   const { loading, setLoading } = React.useState(false);
   const [cocktail, setCocktail] = React.useState(null);
 
+  React.useEffect(() => {
+    setLoading(true);
+    async function getCocktail() {
+      try {
+        const response = await fetch(`${url}${id}`);
+        const data = await response.json();
+        if (data.drinks) {
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1: ingredient1,
+            strIngredient2: ingredient2,
+            strIngredient3: ingredient3,
+            strIngredient4: ingredient4,
+            strIngredient5: ingredient5,
+          } = data.drinks[0];
+        } else {
+          setCocktail(null);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    }
+    getCocktail();
+  }, [id]);
   return (
     <div>
       <h2>Single cocktail page</h2>
