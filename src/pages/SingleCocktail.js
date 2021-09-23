c78a1a23470044076a819702d9d975b0fad06271
Franco Traverso <franco-t20@outlook.com>
2021-09-23 20:26:01 -0300

    add useState hooks for loading and cocktail state

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,6 +1,14 @@
 import React from "react";
+import Loading from "../components/Loading";
+import { useParams, Link } from "react-router-dom";
+
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const SingleCocktail = () => {
+  const { id } = useParams();
+  const { loading, setLoading } = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
+
   return (
     <div>
       <h2>Single cocktail page</h2>
